08bd9462d66d3de7e0341c0407883db82a45852d
leongaban <leongaban@gmail.com>
2023-08-04 17:04:18 +0800

    fetching data & remote strictMode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      monsters: [],
+    }
+
+    console.log('1')
+  }
+
+  componentDidMount() {
+    console.log('3')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users =>
+        this.setState(
+          () => {
+            return { monsters: users }
+          },
+          () => {
+            console.log(this.state)
+          }
+        )
+      )
+  }
+
+  render() {
+    console.log('2')
+    const { monsters } = this.state
+    return (
+      <div className="App">
+        {monsters.map((monster, i) => (
+          <div key={monster.id}>
+            <h1>{monster.name}</h1>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
