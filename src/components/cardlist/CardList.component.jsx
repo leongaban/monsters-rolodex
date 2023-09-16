a0bf799b0221312aa77513f1aff8613106b164d6
leongaban <leongaban@gmail.com>
2023-09-16 09:02:49 -0500

    converted all Classes into components

--- src/components/cardlist/CardList.component.jsx
+++ src/components/cardlist/CardList.component.jsx
@@ -1,20 +1,13 @@
 import { Component } from 'react'
-
 import CardComponent from '../card/Card.component'
 import './card-list.styles.css'
 
-class CardList extends Component {
-  render() {
-    const { monsters } = this.props
-
-    return (
-      <div className='card-list'>
-        {monsters.map(monster => (
-          <CardComponent monster={monster} key={monster.id} />
-        ))}
-      </div>
-    )
-  }
-}
+const CardList = ({ monsters }) => (
+  <div className='card-list'>
+    {monsters.map(monster => (
+      <CardComponent monster={monster} key={monster.id} />
+    ))}
+  </div>
+)
 
 export default CardList
